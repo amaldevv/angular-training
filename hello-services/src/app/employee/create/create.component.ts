5be92c13e3f75c9805b2de7b49833f18a6ea0690
amaldevv <amaldevv@outlook.com>
2017-07-20 12:43:48 +0530

    Validating required with styles

--- hello-services/src/app/employee/create/create.component.ts
+++ hello-services/src/app/employee/create/create.component.ts
@@ -11,7 +11,7 @@ import { Router } from "@angular/router";
 export class EmployeeCreateComponent implements OnInit {
 
   newEmployee:Employee ={
-    Id : 0,
+    Id : null,
      FirstName:'',
      LastName:'',
      City:'',
